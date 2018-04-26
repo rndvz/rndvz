0b246f42ce750a1d9f98f0ec9abe7ac1a1ff9dbd
pmatejko <matejko.pawel@gmail.com>
2018-04-27 01:00:08 +0200

    created spring project skeleton

---
+++ backend/src/main/java/pl/edu/agh/rndvz/RndvzApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.rndvz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RndvzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RndvzApplication.class, args);
+    }
+}
