633c822789f0bd8bce3a9affd40ccec719c8ff89
Mateusz <mateusz14321@gmail.com>
2018-05-23 00:39:35 +0200

    Playing with Angular and spring

--- src/main/java/pl/edu/agh/rndvz/RndvzApplication.java
+++ src/main/java/pl/edu/agh/rndvz/RndvzApplication.java
@@ -2,11 +2,56 @@ package pl.edu.agh.rndvz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @SpringBootApplication
+@RestController
 public class RndvzApplication {
-
+    @RequestMapping("/greeting")
+    public Greeting greeting(){
+        return new Greeting("hello vegas!");
+    }
     public static void main(String[] args) {
         SpringApplication.run(RndvzApplication.class, args);
+    }
+
+}
+
+class Greeting {
+    private String id = UUID.randomUUID().toString();
+    private String msg;
+
+    @SuppressWarnings("unused")
+    private Greeting() {
+    }
+
+    public Greeting(String msg){
+        this.msg = msg;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "msg='" + msg + '\'' +
+                '}';
     }
 }
