f3020f6f5a80dcdf488c8d6151c00f3f2817eda9
Ro6ertWcislo <robert.wcislo1995@gmail.com>
2018-06-24 21:14:16 +0200

    added UserRelations tests

---
+++ src/test/java/pl/edu/agh/rndvz/UserRelationTests.java
@@ -0,0 +1,106 @@
+package pl.edu.agh.rndvz;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import pl.edu.agh.rndvz.model.User;
+import pl.edu.agh.rndvz.persistence.UserRepository;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserRelationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void deleteAllBeforeTests() {
+        userRepository.deleteAll();
+    }
+
+
+    @Test
+    public void shouldAcceptButNotMatch() throws Exception {
+        User user1 = createAndGet("Frodo");
+        User user2 = createAndGet("Gandalf");
+        String content = "{\"from\":" + user1.getId() + ",\"to\":" + user2.getId() + "}";
+
+        mockMvc.perform(post("/accept")
+                .header("Content-Type", "application/json")
+                .content(content))
+                .andExpect(
+                        status().isOk()).andExpect(jsonPath("$.value").value(false));
+
+    }
+
+
+    @Test
+    public void shouldMatch() throws Exception {
+        User user1 = createAndGet("Frodo");
+        User user2 = createAndGet("Gandalf");
+        String content1 = "{\"from\":" + user1.getId() + ",\"to\":" + user2.getId() + "}";
+        String content2 = "{\"from\":" + user2.getId() + ",\"to\":" + user1.getId() + "}";
+
+        mockMvc.perform(post("/accept")
+                .header("Content-Type", "application/json")
+                .content(content1))
+                .andExpect(
+                        status().isOk());
+        mockMvc.perform(post("/accept")
+                .header("Content-Type", "application/json")
+                .content(content2))
+                .andExpect(
+                        status().isOk()).andExpect(jsonPath("$.value").value(true));
+
+    }
+
+    @Test
+    public void shouldBlock() throws Exception {
+        User user1 = createAndGet("Frodo");
+        User user2 = createAndGet("Gandalf");
+        String content1 = "{\"from\":" + user1.getId() + ",\"to\":" + user2.getId() + "}";
+
+        mockMvc.perform(post("/block")
+                .header("Content-Type", "application/json")
+                .content(content1))
+                .andExpect(
+                        status().isOk())
+                .andExpect(jsonPath("$.login").value(user1.getLogin()));
+
+
+    }
+
+
+    private User createAndGet(String login) throws Exception {
+        mockMvc.perform(post("/users").content(
+                "{\"login\": \"" + login + "\"}")).andExpect(
+                status().isCreated()).andReturn();
+
+        MvcResult result1 = mockMvc.perform(get("/users/search/findByLogin/" + login)).andExpect(
+                status().isOk()).andReturn();
+
+        String jsonString = result1.getResponse().getContentAsString();
+        System.out.println(jsonString);
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.readValue(jsonString, User.class);
+    }
+
+
+}
